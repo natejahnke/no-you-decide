81d83bbf17c2b231a4eaf041427da31679a49661
Jahnke <naterased@gmail.com>
2019-11-12 15:35:25 -0600

    still stuck on connecting to graphql api, saving work for now.

--- src/app.js
+++ src/app.js
@@ -1,57 +1,62 @@
 import React from "react";
 // import Autocomplete from "./components/Autocomplete";
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-boost';
-import { setContext } from "apollo-link-context";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+// import { setContext } from "apollo-link-context";
+// import { InMemoryCache } from "apollo-cache-inmemory";
 
-import Stores from './components/Stores';
-import { createHttpLink } from "apollo-link-http";
+import Stores from "./components/Stores";
+// import { get } from "http";
+// import { createHttpLink } from "apollo-link-http";
 
-// const client = new ApolloClient({
-//   uri: 'https://api.yelp.com/v3/graphql',
-//   fetchOptions: {
-//     mode: "no-cors",
-//   },
-//   request: operation => {
-//     operation.setContext({
-//     headers: {
-//       Authorization: 'Bearer rpbJR_5oQtX2nznabKUk01y6bRq4mZ_v7k7Os9vWjYRukdSC--nrSYTzeHXFfd4ojloryKxoKK5MiJv2aBeADWq_LMnU_nQJDrx-fTaBM5KkeCGGV75nZl_D98bJXXYx',
-//     },
-//   });
-// },
-// });
-
-const httpLink = createHttpLink({
+const client = new ApolloClient({
   uri: "https://api.yelp.com/v3/graphql",
-  fetchOptions: {
-    mode: "no-cors"
+  // fetchOptions: {
+  //   mode: "no-cors"
+  // },
+  request: operation => {
+    operation.setContext({
+      headers: {
+        Authorization:
+          "Bearer rpbJR_5oQtX2nznabKUk01y6bRq4mZ_v7k7Os9vWjYRukdSC--nrSYTzeHXFfd4ojloryKxoKK5MiJv2aBeADWq_LMnU_nQJDrx-fTaBM5KkeCGGV75nZl_D98bJXXYx",
+        "Content-Type": "application/graphql",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
   }
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = "rpbJR_5oQtX2nznabKUk01y6bRq4mZ_v7k7Os9vWjYRukdSC--nrSYTzeHXFfd4ojloryKxoKK5MiJv2aBeADWq_LMnU_nQJDrx-fTaBM5KkeCGGV75nZl_D98bJXXYx";
-  return {
-    headers: {
-      ...headers,
-      Authorization: token ? `Bearer ${token}` : "",
-      'Content-Type': "application/graphql"
-    }
-  };
-});
+// const httpLink = createHttpLink({
+//   uri: "https://api.yelp.com/v3/graphql",
+//   fetchOptions: {
+//     mode: "no-cors"
+//   }
+// });
 
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-});
+// const authLink = setContext((_, { headers }) => {
+//   const token = "rpbJR_5oQtX2nznabKUk01y6bRq4mZ_v7k7Os9vWjYRukdSC--nrSYTzeHXFfd4ojloryKxoKK5MiJv2aBeADWq_LMnU_nQJDrx-fTaBM5KkeCGGV75nZl_D98bJXXYx";
+//   return {
+//     headers: {
+//       ...headers,
+//       Authorization: token ? `Bearer ${token}` : "",
+//       'Content-Type': "application/graphql",
+//       'Access-Control-Allow-Origin': "GET, POST, HEAD, OPTIONS, PUT, DELETE, PATCH",
+//     }
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache()
+// });
 
 const App = () => (
   <ApolloProvider client={client}>
-      {/* <Autocomplete
+    {/* <Autocomplete
         suggestions={["White", "Black", "Green", "Blue", "Yellow", "Red"]}
       /> */}
-      <Stores />
+    <Stores />
   </ApolloProvider>
-)
+);
 
 export default App;
